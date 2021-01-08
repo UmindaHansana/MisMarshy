ac0f7eadef4d37f73c63843d173e8e8e8f499732
UHA Dev <72374126+UmindaHansana@users.noreply.github.com>
2021-01-08 12:58:41 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -20,26 +20,20 @@ from haruka.modules.helper_funcs.misc import paginate_modules
 from haruka.modules.translations.strings import tld, tld_help 
 from haruka.modules.connection import connected
 
-PM_START = """Hello {}, my name is {}!
+PM_START = """Hello {}👋, My name is {}🤖
 
-You know how hard it is sometimes to manage group so here is the solution for you
-
-I'm group manager bot and Anti-spam for RR Players🃏
+I'm group manager bot and Anti-spam of Makeit-Mellow Fan base🤗
 
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+Made from the source code of Hakura Aya
 
-Special thanks to [this person](t.me/denver02) for helping me.
+Special thanks to @inukaASiTH for helping me.
 
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
+Click /help or Help button below to find out more about and how to use me to my full potential.
 
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
-
-Click /help or Help button below to find out more about how to use me to my full potential.
-
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+🙏📍📍🙏
 """
 
 
